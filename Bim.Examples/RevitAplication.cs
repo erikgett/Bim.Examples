ea1a04994b7c2da122645a333b2c65504ab435b6
erik_ <erik_@ERIK>
2025-01-29 20:03:20 +0500

    Add project files.

---
+++ Bim.Examples/RevitAplication.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.UI;
+
+namespace Bim.Examples
+{
+    public class RevitAplication
+        : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            int a = 4;
+            int b = 5;
+            int c = a + b;
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+            => Result.Succeeded;
+    }
+}
