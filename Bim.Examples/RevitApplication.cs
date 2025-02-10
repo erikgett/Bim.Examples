f94a83cea0145b3eed4ce0c3f96e520114ad1c46
erik_ <erik_@ERIK>
2025-02-10 21:58:02 +0500

    some refactore

--- Bim.Examples/RevitApplication.cs
+++ Bim.Examples/RevitApplication.cs
@@ -1,33 +1,36 @@
-﻿using Autodesk.Revit.DB;
+﻿// <copyright file="RevitApplication.cs" company="IT4BIM">
+// Copyright (c) IT4BIM. All rights reserved.
+// Licensed under the NC license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System.Collections.Generic;
 
-namespace Bim.Examples
+namespace Bim.Examples;
+
+public class RevitApplication
+    : IExternalApplication
 {
-    public class RevitApplication
-        : IExternalApplication
+    public Result OnStartup(UIControlledApplication application)
     {
-        public Result OnStartup(UIControlledApplication application)
-        {
-            int a = 4;
-            int b = 5;
-            int c = a + b;
-            return Result.Succeeded;
-        }
-
-        public Result OnShutdown(UIControlledApplication application)
-            => Result.Succeeded;
+        int a = 4;
+        int b = 5;
+        int c = a + b;
+        return Result.Succeeded;
     }
 
-    public static class Extensions
+    public Result OnShutdown(UIControlledApplication application)
+        => Result.Succeeded;
+}
+
+public static class Extensions
+{
+    public static long GetValue(this ElementId elementId)
     {
-        public static long GetValue(this ElementId elementId)
-        {
-            #if R17 || R18 || R19 || R20 || R21 || R22 || R23
-            return elementId.IntegerValue;
-            #else
-            return elementId.Value;
-            #endif
-        }
+        #if R17 || R18 || R19 || R20 || R21 || R22 || R23 || R24
+        return elementId.IntegerValue;
+        #else
+        return elementId.Value;
+        #endif
     }
 }
