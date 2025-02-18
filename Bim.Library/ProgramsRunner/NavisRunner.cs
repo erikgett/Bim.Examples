f7c73b96be3a897fedade8a1ad77e0c235b9fdab
erik_ <erik_@ERIK>
2025-02-18 20:15:15 +0500

    add server and start classes for run revit and navis

---
+++ Bim.Library/ProgramsRunner/NavisRunner.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="NavisRunner.cs" company="IT4BIM">
+// Copyright (c) IT4BIM. All rights reserved.
+// Licensed under the NC license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using Autodesk.Navisworks.Api.Automation;
+
+namespace Bim.Library.ProgramsRunner;
+
+/// <summary> Help run instance off NavisWorks and help to execute script to export nwf file.</summary>
+public class NavisRunner
+{
+    private readonly NavisworksApplication navisApp;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NavisRunner"/> class.
+    /// Start NavisWorks application and save it on variable.
+    /// </summary>
+    public NavisRunner()
+    {
+        this.navisApp = new NavisworksApplication
+        {
+            Visible = false,
+        };
+    }
+
+    /// <summary> Using started app off Navisworks and run plugin for export report. </summary>
+    /// <param name="filePath">absolute path to nwf file.</param>
+    /// <param name="commandName">Relative path to command on navis ribbon panel.</param>
+    public void GenerateCollisionReport(string filePath, string commandName = "NavisReportCreater.ClashReport.Report")
+    {
+        try
+        {
+            // open selected file.
+            this.navisApp.OpenFile(filePath);
+
+            // export to xml.
+            this.navisApp.ExecuteAddInPlugin(commandName);
+        }
+        catch
+        {
+            // do nothing.
+        }
+    }
+
+    /// <summary> Kill NavisWorks app instance from task manager. </summary>
+    public void Kill()
+    {
+        try
+        {
+            this.navisApp.Dispose();
+        }
+        catch
+        {
+            // do nothing.
+        }
+    }
+}
---
+++ Bim.Library/ProgramsRunner/RevitRunner.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="RevitRunner.cs" company="IT4BIM">
+// Copyright (c) IT4BIM. All rights reserved.
+// Licensed under the NC license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using System.Diagnostics;
+
+namespace Bim.Library.ProgramsRunner;
+
+/// <summary> Run Revit exe with plugin to export nwc. </summary>
+public class RevitRunner
+{
+    private readonly string pathToRevitExe;
+
+    private readonly string argument;
+
+    private Process revitProcess;
+
+    /// <summary> Initializes a new instance of the <see cref="RevitRunner"/> class.</summary>
+    /// <param name="argument">path to Revit journal.</param>
+    /// <param name="revitVersion">path to Revit exe.</param>
+    public RevitRunner(
+        string argument,
+        string revitVersion = "2021")
+    {
+        this.argument = argument;
+        this.pathToRevitExe = $"C:\\Program Files\\Autodesk\\Revit {revitVersion}\\Revit.exe";
+    }
+
+    /// <summary> Finalizes an instance of the <see cref="RevitRunner"/> class. </summary>
+    ~RevitRunner()
+    {
+        this.Kill();
+    }
+
+    /// <summary> Run Revit and plugin for export from rvt to nwc. </summary>
+    public void Run()
+    {
+        this.revitProcess = new Process();
+        this.revitProcess.StartInfo.FileName = this.pathToRevitExe;
+        this.revitProcess.StartInfo.Arguments = this.argument;
+
+        this.revitProcess.Start();
+    }
+
+    /// <summary> Kill instance off Revit. </summary>
+    public void Kill()
+    {
+        try
+        {
+            this.revitProcess?.Kill();
+        }
+        catch
+        {
+            // do nothing.
+        }
+    }
+}
