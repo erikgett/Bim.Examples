b86df94406c3159b08d34f7a68adca6bc8f79fd6
erik_ <erik_@ERIK>
2025-02-19 23:01:36 +0500

    небольшой рефакторинг

--- OrchestrationExample/Bim.Orchestrator.Server/Program.cs
+++ OrchestrationExample/Bim.Orchestrator.Server/Program.cs
@@ -1,3 +1,4 @@
+using Bim.Orchestrator.Server;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHostedService<RevitBackgroundService>();
 
 var app = builder.Build();
 
---
+++ OrchestrationExample/Bim.Orchestrator.Server/RevitBackgroundService.cs
@@ -0,0 +1,39 @@
+﻿using Bim.Library.ProgramsRunner;
+
+namespace Bim.Orchestrator.Server;
+
+public class RevitBackgroundService : BackgroundService
+{
+    private readonly int revitInstanceCount;
+    private readonly string journalPath;
+    private readonly List<RevitRunner> revitRunners = new();
+
+    public RevitBackgroundService(IConfiguration configuration)
+    {
+        var section = configuration.GetSection("RevitOrchestration");
+        journalPath = section["JournalPath"] ?? throw new ArgumentNullException("JournalPath is not set.");
+        revitInstanceCount = int.TryParse(section["InstanceCount"], out var count) ? count : 3;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        for (int i = 0; i < revitInstanceCount; i++)
+        {
+            var journalFile = Path.Combine(journalPath, $"Runner.txt");
+            var runner = new RevitRunner(journalFile);
+            revitRunners.Add(runner);
+            Task.Run(() => runner.Run(), stoppingToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        foreach (var runner in revitRunners)
+        {
+            runner.Kill();
+        }
+        return base.StopAsync(cancellationToken);
+    }
+}
