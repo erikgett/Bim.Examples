b7c3842ba18025f7bb119da48bf84ec9482f5bfa
erik_ <erik_@ERIK>
2025-03-02 21:39:44 +0500

    add controller for navisworks tasks

---
+++ OrchestrationExample/Bim.Orchestrator.Server/Controllers/NavisTaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace Bim.Orchestrator.Server.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class NavisTaskController : ControllerBase
+{
+    private static readonly ConcurrentQueue<string> filesQueue = new();
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        if (filesQueue.TryDequeue(out var task))
+        {
+            return Ok(new { Message = "Task retrieved", Task = task });
+        }
+        return NotFound(new { Message = "Queue is empty" });
+    }
+
+
+    [HttpPost]
+    public IActionResult Post([FromBody] TaskRequest request)
+    {
+        if (string.IsNullOrEmpty(request.Value))
+        {
+            return BadRequest("Task value is required.");
+        }
+
+        filesQueue.Enqueue(request.Value);
+        return Ok(new { Message = "Task added", Task = request.Value });
+    }
+}
