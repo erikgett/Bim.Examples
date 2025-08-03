2135d1590c680c868529365e2ba62bc6e33e9cb1
erik_ <erik_@ERIK>
2025-08-03 11:14:31 +0500

    add build.cs

---
+++ Build.Library/Build.cs
@@ -0,0 +1,19 @@
+﻿using Nuke.Common;
+using Nuke.Common.ProjectModel;
+
+namespace Build.Library;
+
+public class Build : BasePluginBuild
+{
+    protected override int minorVersion => 0;
+
+    protected override int maintenanceVersion => 2;
+
+    protected override int gitlabProjectId => 605;
+
+
+    [Solution("Bim.Examples.sln")]
+    public override Solution Solution { get; }
+
+    public static int Main() => Execute<Build>(x => x.PushToGitLabNugetRepository);
+}
